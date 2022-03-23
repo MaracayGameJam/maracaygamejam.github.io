64e9ca0cb7b64bf7f87a752e6003f23ef7fed27a
Kevin Esaa <kevin.esaa@gmail.com>
2022-03-23 19:03:47 -0400

    refactor: extract reference id

---
+++ home/js/referencesIds.js
@@ -0,0 +1,13 @@
+const REFERENCE_ID = {
+    homeTitle:"title",
+	logoIcon:"logo_image",
+	facebookHref:"logo_image",
+	facebookImageIcon:"facebook_image",
+	twitterHref:"twitter_link",
+	twitterImageIcon:"twitter_image",
+	instagramHref:"instagram_link",
+	instagramImageIcon:"instagram_image",
+	youtubeHref:"youtube_link",
+	youtubeImageIcon:"youtube_image",
+	emailTextRef:"email",
+};
